e1ea90a835fc825b334ba9b0fd9b4b2ec08e54b0
Lia <a2020114909@estudiantes.upsa.edu.bo>
2021-10-13 18:16:37 -0400

    Se agregó el controlador random con su método

---
+++ Baraja_RubindeCelis/Controllers/RandomController.cs
@@ -0,0 +1,43 @@
+﻿using Baraja_RubindeCelis.Data;
+using Baraja_RubindeCelis.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baraja_RubindeCelis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RandomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Baraja>> GetBaraja()
+        {
+
+            var list = await _context.Baraja.ToListAsync();
+
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+
+            var cancion = list[index];
+
+            if (cancion == null)
+            {
+                return NoContent();
+            }
+
+            return cancion;
+        }
+    }
+}
